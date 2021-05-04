e6d1216ece1c587438b5fcd3af599d5ed13625ed
nicolaslaje <nicolaje35@gmail.com>
2021-05-04 01:06:08 -0300

    Stage 3 Accordion Css & Js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var arrowbtnOn;
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-var arrowbtnOff;
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function () {
+  /* Toggle between adding and removing the "active" class,
+  to highlight the button that controls the panel */
+  this.classList.toggle("active");
 
-function arrowbtnOn () {
- if arrowbtnOn
+  /* Toggle between hiding and showing the active panel */
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+   panel.style.display = "none";
+  } else {
+   panel.style.display = "block";
+  }
+ });
 }
